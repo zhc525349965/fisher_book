05b313179ef0aca736a8f4178058734863d5a2b8
mario.zhang <525349965@qq.com>
2019-09-23 08:14:01 +0800

    3-9

---
+++ app/book/book.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+# Description: 存放视图函数
+#      Author: Mario
+#    Datetime: 2019-09-23 08:09
+from flask import jsonify
+
+from helper import is_isbn_or_key
+from yushu_book import YuShuBook
+
+
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    if isbn_or_key == 'key':
+        result = YuShuBook.search_by_keyword(q)
+
+    return jsonify(result)
--- fisher.py
+++ fisher.py
@@ -4,25 +4,10 @@
 #      Author: Mario
 #    Datetime: 2019-09-22 12:34
 
-from flask import Flask, jsonify
-
-from helper import is_isbn_or_key
-from yushu_book import YuShuBook
+from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-
-@app.route('/book/search/<q>/<page>')
-def search(q, page):
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    if isbn_or_key == 'key':
-        result = YuShuBook.search_by_keyword(q)
-
-    return jsonify(result)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=app.config['DEBUG'])
