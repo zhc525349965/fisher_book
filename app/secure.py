4a224e0f787c1fabf33f2ba387a3b8039119a4a3
mario.zhang <525349965@qq.com>
2019-09-27 23:43:34 +0800

    删除文件

--- app/secure.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-# Description: 存储私密文件
-#      Author: Mario
-#    Datetime: 2019-09-22 13:58
-
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@localhost:3306/new_fisher'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
