1a9a9177497ea01d756d79a6a28fcb2c5d2af959
mario.zhang <525349965@qq.com>
2019-10-06 22:05:08 +0800

    10-1

---
+++ app/models/wish.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+# Description: $Description$
+#      Author: Mario
+#    Datetime: 2019-10-06 21:27
+
+from app.models.base import Base
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+
+class Wish(Base):
+    id = Column(Integer, primary_key=True)
+    launched = Column(Boolean, default=False)
+
+    # 用于对应user模型，指定这本书是那个用户赠予
+    user = relationship('User')
+    uid = Column(Integer, ForeignKey('user.id'))
+
+    # 关联书籍isbn
+    # 字符串类型，长度15 不可为空
+    isbn = Column(String(15), nullable=False)
--- app/web/gift.py
+++ app/web/gift.py
@@ -2,8 +2,12 @@
  Created by 七月 on 2018/1/26.
  微信公众号：林间有风
 """
+from flask import current_app
+
+from app.models.base import db
+from app.models.gift import Gift
 from app.web.create_blueprint import web
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 @web.route('/my/gifts')
@@ -14,7 +18,14 @@ def my_gifts():
 
 
 @web.route('/gifts/book/<isbn>')
+@login_required
 def save_to_gifts(isbn):
+    gift = Gift()
+    gift.isbn = isbn
+    gift.uid = current_user.id
+    current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
+    db.session.add(gift)
+    db.session.commit()
     pass
 
 
