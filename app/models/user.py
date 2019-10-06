8c3473eaec8bab3a579a9c26de2acbf95fcc384a
mario.zhang <525349965@qq.com>
2019-10-06 22:25:28 +0800

    10-2

--- app/models/user.py
+++ app/models/user.py
@@ -3,11 +3,15 @@
 # Description: $Description$
 #      Author: Mario
 #    Datetime: 2019-10-05 11:43
+from app.libs.helper import is_isbn_or_key
 from app.models.base import Base
 from sqlalchemy import Column, Integer, String, Boolean, Float
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login_manager
+from app.models.gift import Gift
+from app.models.wish import Wish
+from app.spider.yushu_book import YuShuBook
 
 
 class User(Base, UserMixin):
@@ -44,6 +48,22 @@ class User(Base, UserMixin):
 
     def check_password(self, raw):
         return check_password_hash(self._password, raw)
+
+    def can_save_to_wish_list(self, isbn):
+        if is_isbn_or_key(isbn) != 'isbn':
+            return False
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(isbn)
+        if not yushu_book.first:
+            return False
+
+        gifting = Gift.query.filter_by(uid=self.id, isbn=isbn, launched=False).first()
+        wishing = Wish.query.filter_by(uid=self.id, isbn=isbn, launched=False).first()
+
+        if not gifting and not wishing:
+            return True
+        else:
+            return False
 
 
 @login_manager.user_loader
