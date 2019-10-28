664ce4810aad4c88fe9f8559b45d3cbc53cb41ea
mario.zhang <525349965@qq.com>
2019-10-28 22:49:07 +0800

    11-1

--- app/models/gift.py
+++ app/models/gift.py
@@ -3,6 +3,8 @@
 # Description: $Description$
 #      Author: Mario
 #    Datetime: 2019-10-05 11:44
+from flask import current_app
+
 from app.models.base import Base
 from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
 from sqlalchemy.orm import relationship
@@ -19,3 +21,8 @@ class Gift(Base):
     # 关联书籍isbn
     # 字符串类型，长度15 不可为空
     isbn = Column(String(15), nullable=False)
+
+    def recent(self):
+        recent_gift = Gift.query.filter_by(launched=False).group_by(Gift.isbn).order_by(Gift.create_time).limit(
+            current_app.config['RECENT_BOOK_COUNT']).distinct().all()
+        return recent_gift
