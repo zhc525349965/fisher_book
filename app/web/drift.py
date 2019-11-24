8d36a2c09034ecc28c9aea6db6ce9813585050b7
mario.zhang <525349965@qq.com>
2019-11-24 11:47:18 +0800

    12-16

--- app/web/drift.py
+++ app/web/drift.py
@@ -2,10 +2,16 @@
  Created by 七月 on 2018/1/26.
  微信公众号：林间有风
 """
-from flask import flash, redirect, url_for, render_template
+from flask import flash, redirect, url_for, render_template, request
 from flask_login import login_required, current_user
+from sqlalchemy import desc, or_
 
+from app.forms.book import DriftForm
+from app.libs.email import send_mail
+from app.models.base import db
+from app.models.drift import Drift
 from app.models.gift import Gift
+from app.view_models.book import BookViewModel
 from app.web.create_blueprint import web
 
 
@@ -22,12 +28,21 @@ def send_drift(gid):
     if not can:
         return render_template('not_enough_beans.html', beans=current_user.beans)
 
+    form = DriftForm(request.form)
+    if request.method == 'POST' and form.validate():
+        save_drift(form, current_gift)
+        send_mail(current_gift.user.email, '有人想要一本书', 'email/get_gift.html', wisher=current_user, gift=current_gift)
+        return render_template(url_for('web.pending'))
+
     gifter = current_gift.user.summary
-    return render_template('drift.html', gifter=gifter, user_beans=current_user.beans)
+    return render_template('drift.html', gifter=gifter, user_beans=current_user.beans, form=form)
 
 
 @web.route('/pending')
+@login_required
 def pending():
+    drifts = Drift.query.filter(or_(Drift.requester_id == current_user.id, Drift.gifter_id == current_user.id)).order_by(
+        desc(Drift.create_time)).all()
     pass
 
 
@@ -44,3 +59,25 @@ def redraw_drift(did):
 @web.route('/drift/<int:did>/mailed')
 def mailed_drift(did):
     pass
+
+
+def save_drift(drift_form, current_gift):
+    with db.auto_commit():
+        drift = Drift()
+        drift_form.populate_obj(drift)
+
+        drift.gift_id = current_gift.id
+        drift.requester_id = current_user.id
+        drift.requester_nickname = current_user.nickname
+        drift.gifter_nickname = current_gift.user.nickname
+        drift.gifter_id = current_gift.user.id
+
+        book = BookViewModel(current_gift.book)
+        drift.book_title = book.title
+        drift.book_author = book.author
+        drift.book_img = book.image
+        drift.isbn = book.isbn
+
+        current_user.beans -= 1
+
+        db.session.add(drift)
