5bb28148daa02016048a0cd4134a6689096d9005
mario.zhang <525349965@qq.com>
2019-11-27 08:49:27 +0800

    跟新密码

--- app/secure.py
+++ app/secure.py
@@ -5,7 +5,7 @@
 #    Datetime: 2019-09-22 13:58
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@localhost:3306/new_fisher'
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:8ik,)P:?@localhost:3306/new_fisher'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = '44a342d702d8bb15df28893c421c3a0f'  # 我爱北京天安门，天安门上太阳升
 
